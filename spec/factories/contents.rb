90129fee090677b87a7842afd606c057a9492e9e
LindaKadz <achielinda44@gmail.com>
2019-01-29 15:42:55 +0300

    initialize content test files

---
+++ spec/factories/contents.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :content do
+    title { "MyString" }
+    published_date { "MyString" }
+    author { "MyString" }
+    summary { "MyString" }
+    content { "MyString" }
+    status { "MyString" }
+  end
+end
