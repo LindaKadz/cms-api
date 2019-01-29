2ad27f8ec7bfae7c16a204e13287286dc297c61e
LindaKadz <achielinda44@gmail.com>
2019-01-29 15:42:06 +0300

    modify test for content

--- spec/requests/api/v0/contents_spec.rb
+++ spec/requests/api/v0/contents_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Api::V0::ContentsController, type: :request do
-  describe 'GET /v0/content' do
-    it 'should return Created!' do
-      get '/api/v0/content'
+  describe 'GET /v0/contents' do
+    it 'should return Present!' do
+      get '/api/v0/contents'
 
       json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
-      expect(json_response['message']).to eq 'Created!'
+      expect(json_response['message']).to eq 'Present!'
     end
   end
 end
