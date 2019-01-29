704362637f01992c7971c0bae87d86d906a45308
LindaKadz <achielinda44@gmail.com>
2019-01-29 15:40:51 +0300

    add contents routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v0 do
+      resources :contents, only: [:index], constraints: { format: 'json' }
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
