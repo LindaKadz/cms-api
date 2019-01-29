3c65fb046ca0615a9ae5d88c157f15f52a9ccee3
LindaKadz <achielinda44@gmail.com>
2019-01-29 05:04:19 +0300

    fix spelling error and syntax error

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,17 @@ module CmsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+   #this is to allow REST actions from any API or source
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+         headers: :any,
+         methods: %w(:get, :put, :post, :delete),
+         expose: %w(access-token expiry token-type uid client),
+         max_age: 0
+       end
+    end
   end
 end
