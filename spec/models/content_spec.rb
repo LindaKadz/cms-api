edb8484c11d24c6a05666df8ea8979cdb5bce2f9
LindaKadz <achielinda44@gmail.com>
2019-01-30 07:44:41 +0300

    test the content model

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'database table' do
+    it { is_expected.to have_db_column(:title) }
+    it { is_expected.to have_db_column(:published_date) }
+    it { is_expected.to have_db_column(:summary) }
+    it { is_expected.to have_db_column(:author) }
+    it { is_expected.to have_db_column(:content) }
+    it { is_expected.to have_db_column(:status) }
+  end
+
+  describe 'relations' do
+    it { is_expected.to belong_to(:user) }
+  end
 end
