d14dc077ff498afd1c3c394b434b58f6aaf24dfb
LindaKadz <achielinda44@gmail.com>
2019-01-30 07:42:14 +0300

    add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v0 do
       resources :contents, only: [:index], constraints: { format: 'json' }
+    end
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
