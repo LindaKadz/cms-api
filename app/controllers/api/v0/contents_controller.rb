a212ca59f8dc88c546f383383124b2b7c28460b5
LindaKadz <achielinda44@gmail.com>
2019-01-30 16:04:36 +0300

    build create action for creating posts and modify the index action to render all posts

--- app/controllers/api/v0/contents_controller.rb
+++ app/controllers/api/v0/contents_controller.rb
@@ -1,5 +1,24 @@
 class Api::V0::ContentsController < ApplicationController
+  before_action :authenticate_api_v1_user!
+
   def index
-    render json: { message: 'Present!' }
+    @content = current_api_v1_user.content
+    render json: { posts: @content}
+  end
+
+  def create
+    @content = Content.new(content_params.merge(user: current_api_v1_user))
+
+    if @content.save
+      render json: { message: 'saved' }
+    else
+      render json: { error: @content.errors.full_messages }
+    end
+  end
+
+  private
+
+  def content_params
+    params.permit(:author, :title, :summary, :content, :status, :published_date)
   end
 end
