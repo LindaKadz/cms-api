fb2462096dfedeca25b01ff29da5733f6354182c
LindaKadz <achielinda44@gmail.com>
2019-01-30 16:05:34 +0300

    add index on schema

--- db/migrate/20190129163010_create_contents.rb
+++ db/migrate/20190129163010_create_contents.rb
@@ -1,7 +1,7 @@
 class CreateContents < ActiveRecord::Migration[5.2]
   def change
     create_table :contents do |t|
-      t.references :user, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.string :title
       t.string :published_date
       t.string :author
