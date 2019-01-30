cb927be3a23f045dcad0b56dae17d0f4c12a56db
LindaKadz <achielinda44@gmail.com>
2019-01-30 07:43:07 +0300

    add contents table

---
+++ db/migrate/20190129163010_create_contents.rb
@@ -0,0 +1,15 @@
+class CreateContents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contents do |t|
+      t.references :user, foreign_key: true
+      t.string :title
+      t.string :published_date
+      t.string :author
+      t.string :summary
+      t.string :content
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_125114) do
+ActiveRecord::Schema.define(version: 2019_01_29_163010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contents", force: :cascade do |t|
+    t.bigint "user_id"
     t.string "title"
     t.string "published_date"
     t.string "author"
@@ -24,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_01_29_125114) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contents_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 2019_01_29_125114) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "contents", "users"
 end
